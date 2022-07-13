6d0901a05c7dea4e2797eff6ecc22ffd6e0a5dba
Shahul <shahulhameedk479@gmail.com>
2022-07-13 22:29:46 +0530

    09-SDD-Website-upto-Mobile-navigation-Menu

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Mobile Navigation
+
+const mobileNav = document.querySelector(".mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const mobileMenuItems = document.querySelectorAll(".mobile-nav .menu-items li");
+
+menuIcon.addEventListener("click", () => {
+	mobileNav.classList.toggle("active");
+});
+
+mobileMenuItems.forEach((i) => {
+	i.addEventListener("click", () => {
+		mobileNav.classList.remove("active");
+	});
+});
+
+// Desktop Navigation
+
 const options = {
 	threshold: 0.8,
 };
